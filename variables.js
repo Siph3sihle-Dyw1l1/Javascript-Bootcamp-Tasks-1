b5088f5ecf3d27f2a8929a09d98eebf9a5164b93
Siphesihle_Dywili <sphesh959@gmail.com>
2026-05-27 11:00:23 +0200

    Variable task

---
+++ variables.js
@@ -0,0 +1,31 @@
+//Exercise 1
+let name = "Siphesihle";
+let age = 25;
+let isStudent = true;
+
+console.log("Name :" + name +
+    "\nAge : " + age +
+    "\nStudent Status: " + isStudent);
+
+//Exercise 2
+let favoriteColour = "Black";
+
+console.log("My favorite colour is " + favoriteColour);
+
+favoriteColour = "White";
+
+console.log("My second favorite colour is " + favoriteColour);
+
+// Exercise 3
+let num1 = 20;
+let num2 = 10;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum = " + sum);
+console.log("Difference = " + difference);
+console.log("Product = " + product);
+console.log("Quotient = " + quotient);
