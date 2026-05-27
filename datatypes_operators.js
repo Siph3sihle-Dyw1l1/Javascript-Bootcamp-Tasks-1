a6210b6a49d025c0388cf4ba6a083a43b35ef8d3
Siphesihle_Dywili <sphesh959@gmail.com>
2026-05-27 11:15:51 +0200

    .

---
+++ datatypes_operators.js
@@ -0,0 +1,19 @@
+// Exercise 1
+const num1 = 20;
+const num2 = 2.5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let modulus = num1 % num2;
+let exponents = num1 ** num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Modulus:", modulus);
+console.log("Exponents:", exponents);
+
+// Exercise 2
