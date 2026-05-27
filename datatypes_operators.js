097eeb3b9e7a2d1c7dcd234703fbdca740525432
Siphesihle_Dywili <sphesh959@gmail.com>
2026-05-27 11:32:09 +0200

    task completed

--- datatypes_operators.js
+++ datatypes_operators.js
@@ -17,3 +17,35 @@ console.log("Modulus:", modulus);
 console.log("Exponents:", exponents);
 
 // Exercise 2
+let results = 6 < 3;
+
+const x = 8;
+const y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+
+const a = true;
+const b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+p += 2;
+console.log(p);
+
+p -= 1;
+console.log(p);
+
+p *= 7;
+console.log(p);
+
+p /= 3;
+console.log(p);
+
+p %= 2;
+console.log(p);
