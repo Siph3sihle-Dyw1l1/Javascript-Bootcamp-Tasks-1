2ae805ff961788e02a5758edbc924e5f9613e580
Siphesihle_Dywili <sphesh959@gmail.com>
2026-05-27 12:11:10 +0200

    Exercise 2 Divisibility Check

--- conditionalStatements_loops.js
+++ conditionalStatements_loops.js
@@ -11,7 +11,7 @@ if (temp < 0) {
 }
 
 
-switch (temp) {
+switch (true) {
     case temp < 0:
         console.log("It's freezing!");
         break;
@@ -26,3 +26,30 @@ switch (temp) {
 }
 
 //Exercise 2:
+const num = 10;
+
+if (num % 2 == 0) {
+    console.log("Divisible by 2.");
+} else if (num % 3 == 0) {
+    console.log("Divisible by 3.");
+} else if (num % 2 == 0 && num % 3 == 0){
+    console.log("Divisible by both.");
+}else{
+    console.log("Not divisible by 2 or 3.");
+}
+
+switch(true){
+    case num % 2 == 0:
+        console.log("Divisible by 2.");
+        break;
+    case num % 3 == 0:
+        console.log("Divisible by 3.");
+        break;
+    case num % 2 == 0 && num % 3 == 0:
+        console.log("Divisible by both.");
+        break;
+    default:
+        console.log("Not divisible by 2 or 3.");
+}
+
+// Exercise 3
