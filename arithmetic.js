4bb32d95263c11f4b0d6b5dc307e86c335fbaa4c
Siphesihle_Dywili <sphesh959@gmail.com>
2026-05-27 09:33:17 +0200

    Exercise 3

---
+++ arithmetic.js
@@ -0,0 +1,12 @@
+let num1 =10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
